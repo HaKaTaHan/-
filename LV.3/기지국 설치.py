81bc1f7a18dd03b6aebc226a8bd11f48792bd210
Kim <rnlcksgrp1@naver.com>
2020-09-03 12:17:03 +0900

    LV.3

--- LV.3/기지국 설치.py
+++ LV.3/기지국 설치.py
@@ -3,21 +3,17 @@ from math import ceil
 def solution(n, stations, w):
     answer = 0
     cover = w*2+1
-    notCoverArr = []
     beforeMaxCover = 0
     
     for i in range(len(stations)):
         leftCover = stations[i] - w - 1
         if i == 0:
             beforeMaxCover = stations[i] + w
-            notCoverArr.append(ceil((leftCover)/cover))
+            answer += ceil((leftCover)/cover)
             continue
-
-        notCoverArr.append(ceil((leftCover - beforeMaxCover)/cover))
+        
+        answer += ceil((leftCover - beforeMaxCover)/cover)
         beforeMaxCover = stations[i] + w
-    
     if beforeMaxCover < n:
-        notCoverArr.append(ceil((n - beforeMaxCover)/cover))
-    
-    answer = sum(notCoverArr)
+        answer += ceil((n - beforeMaxCover)/cover)
     return answer
